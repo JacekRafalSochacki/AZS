c49a35d408721c348a51dedd8538bc5357847fd2
SoJaRa <jacek.sochacki007@gmail.com>
2019-02-24 12:40:03 +0100

    Menu reagujace na przewijanie

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {alerting} from "C:/Users/Pancio/WebstormProjects/AZS_lyzwy/AZS/src/assets/js/menu.animation.js"
 
-declare function alerting(): any;
 
 @Component({
   selector: 'app-menu',
@@ -12,6 +12,23 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    window.onscroll= function() {
+      growShrinkLogo();
+    };
+
+    var lastScroll=0;
+    function growShrinkLogo() {
+      var Logo = document.getElementById("logo");
+      if ( document.documentElement.scrollTop  > lastScroll) {
+        Logo.style.height = '50px';
+      } else {
+        Logo.style.height = '100px';
+      }
+      lastScroll=document.documentElement.scrollTop;
+    }
+
+
   }
 
 }
