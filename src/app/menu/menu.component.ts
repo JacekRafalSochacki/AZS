b26879fd336ac8ec1f86a1544f63a747779e50f0
SoJaRa <jacek.sochacki007@gmail.com>
2019-02-24 12:22:42 +0100

    Hamburger menu

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+declare function alerting(): any;
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
