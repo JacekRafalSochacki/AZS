ac514ade08de8669f89a45d4dd721708b555c8b0
SoJaRa <jacek.sochacki007@gmail.com>
2019-03-02 16:12:07 +0100

    Przyciecie drzewka commitow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MainComponent,
     FooterComponent
   ],
   imports: [
