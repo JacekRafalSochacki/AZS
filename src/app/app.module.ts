8f92f8353fce221c84f4965dec991be6cb1d85a0
SoJaRa <jacek.sochacki007@gmail.com>
2019-03-02 15:20:36 +0100

    Dodanie footera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
